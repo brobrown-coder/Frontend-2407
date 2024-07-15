45cc5882a9f95b4fecda7455e2d9891bd98c3c71
Seungheon Baek <deon102@naver.com>
2024-07-15 12:36:42 +0900

    javascript

--- JS/ch02.자료와변수/01.자료형.js
+++ JS/ch02.자료와변수/01.자료형.js
@@ -21,9 +21,36 @@
  // 문자열: 속성(attribute), method
  console.log(hello.length, hello.charAt(0), hello.charAt(3));
 
- let a = 'this is `string`'
- console.log(a);
+ let c = 'this is `string`'
+ console.log(c);
 
  // template literal
  console.log(hello + '문자열의 길이는' + hello.length + '입니다.');
  console.log(`${hello} 문자열의 길이는 ${hello.length}입니다.`);
+
+ // 2. 숫자(Number)
+ // 자바 스크립트에서는 정수형(integer), 실수형(floating point)을 구분하지 않는다.
+ let a = 3, b = 4;
+ console.log(a+b, a-b, a*b, a/b, b%a);
+ let pi = 0.314159e1        // 0.314159 * 10
+ let f = 1.3e-8;        //1.3 * 10의 -8승
+ console.log(pi,f);
+ console.log(2 ** 10, 2 ** 20, 2 ** 30);    // 
+
+ // 3. 논리형(Boolean) - true, false
+ // 비교연산
+ console.log(a > b, 10 === 10.0);       // === : 타입과 값이 같을 때 참
+ console.log(10 == '10', 10 === '10');      //true, false
+ // 논리연상
+ let x = 10;
+ console.log(x > 8 || x < 0);
+ console.log(x > 0 && x < 8);
+ console.log(x);
+
+ // 자료형 검사
+ console.log(typeof hello, typeof(a), typeof(true));    // string, number, boolean
+ console.log(typeof console); //object
+ console.log(typeof let); // undefined
+
+ let y = a + -b;        // + : binary perator, - : unary perator
+ console.log(y);
